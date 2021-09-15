e62ae3435a21242d18efd7a91e8de690348d8f5a
Alexandre <alexandrepedroso1991@hotmail.com>
2021-09-15 13:39:47 -0300

    ContaCorrente em CSharp

---
+++ AppConta/ContaCorrente.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConta
+{
+    class ContaCorrente
+    {
+        private int Conta;
+        private string Correntista;
+        private double Saldo;
+
+        public int Conta1 { get => Conta; set => Conta = value; }
+        public string Correntista1 { get => Correntista; set => Correntista = value; }
+        public double Saldo1 { get => Saldo; set => Saldo = value; }
+
+        public ContaCorrente()
+        {
+             
+        }
+
+        public ContaCorrente(int Conta, string Correntista, float Saldo)
+        {
+            this.setConta(Conta);
+            this.setCorrentista(Correntista);
+            this.setSaldo(Saldo);
+        }
+
+        public void setConta(int Conta)
+        {
+            this.Conta1 = Conta;
+        }
+
+        public int getConta(int Conta)
+        {
+            return this.Conta1;
+        }
+
+        public void setCorrentista(string Correntista)
+        {
+            this.Correntista1 = Correntista;
+        }
+
+        public string getCorrentista(string Correntista)
+        {
+            return this.Correntista1;
+        }
+
+        public void setSaldo(double Saldo)
+        {
+            this.Saldo1 = Saldo;
+        }
+
+        public double getSaldo(double Saldo)
+        {
+            return this.Saldo1;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.setSaldo(this.getSaldo(this.Saldo1) + valor);
+        }
+
+        public bool Saque(double valor)
+        {
+            if (this.getSaldo(this.Saldo1) >= valor)
+            {
+                this.setSaldo(this.getSaldo(this.Saldo1) - valor);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AppConta/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace AppConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente C1 = new ContaCorrente();
+
+            C1.Correntista1 = "Wendell";
+            C1.Conta1 = 055812;
+            C1.Saldo1 = 000;
+            double valor;
+            int op = 1;
+          
+
+            Console.WriteLine("Olá, " + C1.Correntista1 + "\n");
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Escolha a operação:\n1 - Depositar\n2 - Sacar\n3 - Saldo\n0 - Sair");
+                op = Convert.ToInt16(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Digite o valor do depósito R$: ");
+                            valor = Convert.ToDouble(Console.ReadLine());
+                            C1.Depositar(valor);
+
+                            Console.WriteLine("Deposito realizado com sucesso!\n ");
+                            Console.WriteLine("Seu Saldo é: " + C1.getSaldo(C1.Saldo1) + "\n");
+
+                            Console.WriteLine("Digite qualquer tecla para voltar ao menu anterior! ");
+                            Console.ReadKey();
+
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Digite o Valor do Saque R$: ");
+                            valor = Convert.ToDouble(Console.ReadLine());
+
+                            if (C1.Saque(valor))
+                            {
+                                Console.WriteLine("Saque efetuado com sucesso!\n ");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Operação não efetuada, Saldo insulficiente!\n ");
+                            }
+                            Console.WriteLine("Digite qualquer tecla para voltar ao menu anterior! ");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Correntista .............. " + C1.Correntista1 + "\n");
+                            Console.WriteLine("Conta       .............. " + C1.Conta1 + "\n");
+                            Console.WriteLine("Saldo       .............. " + C1.Saldo1 + "\n");
+
+                            Console.WriteLine("Digite qualquer tecla para voltar ao menu anterior! ");
+                            Console.ReadKey();
+
+                        }
+                        break;
+                    case 0:
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine("Obrigado por usar o Banco!\n ");
+
+                            Console.WriteLine("Digite qualquer tecla para sair");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Escolha uma opção válida de 0 a 3!");
+                        Console.WriteLine("Digite qualquer tecla para sair");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (op != 0);
+            
+        }
+    }
+}
